62d56fdce492eefd4973feeb3348256566cded25
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-08-22 01:03:45 +0300

    ders20

---
+++ ApiProjeKampi.WebArayuz/ViewComponents/_HeadDefaultCompanentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebArayuz.ViewComponents
+{
+    public class _HeadDefaultCompanentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApiProjeKampi.WebArayuz/ViewComponents/_NavbarDefaultCompanentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebArayuz.ViewComponents
+{
+    public class _NavbarDefaultCompanentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
